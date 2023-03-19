cd87332848693414d089a63f19429b11c9589a09
Anurag Gupta <guptaanurag@salesforce.com>
2023-03-19 17:08:02 +0530

    Changes

--- terraformcodeazure/ansible.sh
+++ terraformcodeazure/ansible.sh
@@ -1,1 +1,7 @@
-ansible-playbook -i "thoughtworks@${1}," --private-key "$(terraform output -raw private_key)" --extra-vars "host_key_checking=False" ../ansiblecode/thoughtworks_role.yml
+
+echo 'private key is' 
+echo "${terraform output -raw private_key}"
+key=$(terraform output -raw private_key)
+ansible-playbook -i "thoughtworks@${1}," --private-key id_rsa --extra-vars "host_key_checking=False" ../ansiblecode/thoughtworks_role.yml
+
+ansible-playbook -i "thoughtworks@${1}," --private-key "${key}" --extra-vars "host_key_checking=False" ../ansiblecode/thoughtworks_role.yml
