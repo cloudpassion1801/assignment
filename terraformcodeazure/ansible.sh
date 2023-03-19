399399ba163423c86615faf0b468ede65545ed00
Anurag Gupta <guptaanurag@salesforce.com>
2023-03-19 15:29:09 +0530

    Test

--- terraformcodeazure/ansible.sh
+++ terraformcodeazure/ansible.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i "thoughtworks@${1},"  --extra-vars "ansible_password=Thoughtworks@1123" ../ansiblecode/thoughtworks_role.yml
+ansible-playbook -i "thoughtworks@${1}," --private-key "${2}" ../ansiblecode/thoughtworks_role.yml
