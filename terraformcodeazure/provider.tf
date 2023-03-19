130acec38e43b63d1f2ed8a16b43ee08f3b052a9
Anurag Gupta <guptaanurag@salesforce.com>
2023-03-19 15:30:02 +0530

    Test

--- terraformcodeazure/provider.tf
+++ terraformcodeazure/provider.tf
@@ -5,12 +5,12 @@ terraform {
       version = "=3.0.0"
     }
   }
-  # backend "azurerm" {
-  #   resource_group_name  = "thoughtworkstfstate"
-  #   storage_account_name = "thoughtworksbackends"
-  #   container_name       = "terraform-tfstate"
-  #   key                  = "terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "thoughtworkstfstate"
+    storage_account_name = "thoughtworksbackends"
+    container_name       = "terraform-tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 # Configure the Microsoft Azure Provider
